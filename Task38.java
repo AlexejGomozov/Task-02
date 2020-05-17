cee6079dade078b5b4a4a51c92306dfeff0ab8ea
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-05-17 10:43:48 +0200

    Add files via upload

---
+++ Task38.java
@@ -0,0 +1,13 @@
+package by.htp.branchings.logic;
+
+public class Task38 {
+public static void task () {
+	//Вычислить значение функции.
+	int x, f;
+	
+	x = 5;
+	
+	if (x >=0 & x<=3) {f = x*x; System.out.print(f);}
+	else if (x>3) {f = 4; System.out.print(f);}
+}
+}
